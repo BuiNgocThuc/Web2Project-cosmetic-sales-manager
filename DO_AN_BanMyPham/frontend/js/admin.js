fd3d51e879f90b18af4f71dca36a3b7a888f0400
Bui Ngoc Thuc <buingocthuc1512@gmail.com>
2023-03-17 13:25:19 +0700

    new 17/3

--- DO_AN_BanMyPham/frontend/js/admin.js
+++ DO_AN_BanMyPham/frontend/js/admin.js
@@ -1,9 +1,82 @@
 var stt = document.querySelector('.switch');
 
-stt.addEventListener('click', function() {
-    if(stt.getAttribute('data-content') === "ngừng hoạt động") {
+stt.addEventListener('click', function () {
+    if (stt.getAttribute('data-content') === "ngừng hoạt động") {
         stt.setAttribute('data-content', 'đang hoạt động')
-    }else {
+    } else {
         stt.setAttribute('data-content', 'ngừng hoạt động')
     }
 })
+
+
+// Fix Width ScrollBar
+
+$(document).ready(function () {
+    checkScrollBars();
+});
+
+function checkScrollBars() {
+    if (checkHasScrollBar()) {
+        let a = getScrollBarWidth();
+        $(".main-content").css("width", "calc(82vw - " + a + "px)");
+        $("header").css("width", "calc(82vw - " + a + "px)");
+    }
+}
+function getScrollBarWidth() {
+    let $outer = $("<div>")
+        .css({ visibility: "hidden", width: 100, overflow: "scroll" })
+        .appendTo("body"),
+        widthWithScroll = $("<div>")
+            .css({ width: "100%" })
+            .appendTo($outer)
+            .outerWidth();
+    $outer.remove();
+    return 100 - widthWithScroll;
+}
+
+function checkHasScrollBar() {
+    if ($(document).height() > $(window).height()) return true;
+    return false;
+}
+
+
+//hide form search
+
+$(document).ready(function () {
+    $(".fa-regular").click(function () {
+        $(".fix-info").slideToggle(500);
+        console.log("In here");
+    });
+});
+
+function changeIconArrow(x) {
+    x.classList.toggle("fa-angle-up");
+    x.classList.toggle("fa-angle-down");
+}
+
+function changeIconMode(x) {
+    x.classList.toggle("fa-sun-bright");
+    x.classList.toggle("fa-moon-stars");
+    var color = $("html").css("--bg")
+    color
+}
+
+
+//select function on sidebar 
+
+$(document).ready(function() {
+    $("#changeMode").click(function() {
+        console.log(this);
+        if(this.classList.contains("fa-moon-stars")){
+            document.documentElement.style.setProperty('--main', '#00EAFF');
+            document.documentElement.style.setProperty('--bg', '#FFFBF1');
+            document.documentElement.style.setProperty('--bg-body', '#e5e5e5');
+            document.documentElement.style.setProperty('--font-color', '#0b1218');
+        }else {
+            document.documentElement.style.setProperty('--main', 'orange');
+            document.documentElement.style.setProperty('--bg', '#0B1218');
+            document.documentElement.style.setProperty('--bg-body', '#131D28');
+            document.documentElement.style.setProperty('--font-color', '#FFFBF1');
+        }
+    });
+});
---
+++ DO_AN_BanMyPham/php/discount.php
@@ -0,0 +1,172 @@
+<div class="sidebar">
+    <div class="brand">
+        <span><img src="../image/LOGO.jpg" alt=""></span>
+        <h2>MOON COSMETIC</h2>
+    </div>
+    <div class="sidemenu">
+        <div class="user">
+            <div class="user__avatar">
+                <img src="https://yt3.ggpht.com/yti/AHXOFjWnPpZwPaRrVu9jjJjZwxgIEnM5msXmhxZH_j90=s88-c-k-c0x00ffffff-no-rj-mo">
+            </div>
+            <div class="user__info">
+                <h3>Bùi Ngọc Thức</h3>
+                <small>Admin</small>
+            </div>
+        </div>
+        <div class="list-function">
+            <ul>
+                <li>
+                    <a href="">
+                        <span><i class="fa-thin fa-house"></i> Trang Chủ</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span><i class="fa-thin fa-bag-shopping"></i> Sản phẩm</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span><i class="fa-thin fa-receipt"></i> Đơn hàng</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span><i class="fa-thin fa-users"></i> Tài Khoản</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="" class="active">
+                        <span><i class="fa-thin fa-badge-percent"></i> Khuyến Mại</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span><i class="fa-thin fa-signal-bars"></i> Thống Kê</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span><i class="fa-thin fa-display"></i> Hiện Thị</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <span><i class="fa-thin fa-circle-user"></i> Quản Trị Người Dùng</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+<div class="main-content">
+    <!-- HEADER -->
+    <header>
+        <div class="title">
+            <div class="menu-icon"><i class="fa-light fa-bars-sort"></i></div>
+            <span class="current-page">Danh sách phiếu giảm giá</span>
+        </div>
+        <div class="tool-icon">
+            <i class="fa-thin fa-sun-bright"></i>
+            <i class="fa-thin fa-moon-stars"></i>
+            <i class="fa-thin fa-bell"></i>
+            <i class="fa-thin fa-gear"></i>
+        </div>
+    </header>
+    <!-- MAIN  -->
+    <main>
+        <div class="form-container">
+            <div>
+                <h3 for="">tùy chọn tìm kiếm</h3>
+                <span><i class="fa-regular fa-angle-up"></i></span>
+            </div>
+            <div>
+                <label for="">Mã</label>
+                <input class="textfield" type="text">
+            </div>
+            <div>
+                <label for="">Mô Tả</label>
+                <input class="textfield" type="text">
+            </div>
+            <div>
+                <label for="">Ngày bắt đầu</label>
+                <input class="textfield" type="date">
+            </div>
+            <div>
+                <label for="">Ngày kết thúc</label>
+                <input class="textfield" type="date">
+            </div>
+            <div>
+                <label for="">Trạng Thái</label>
+                <input type="checkbox" class="switch" data-content="ngừng hoạt động">
+            </div>
+            <div>
+                <label for=""></label>
+                <button class="btn btn--Search">Tìm Kiếm</button>
+                <button class="btn btn--Undo">Hoàn Tác</button>
+            </div>
+        </div>
+        <div class="list-container">
+            <div class="title-list">
+                <h3>danh sách phiếu giảm giá</h3>
+                <button>
+                    <i class="fa-light fa-plus"></i>
+                    <a href="">tạo mới</a>
+                </button>
+            </div>
+            <div class="list-code">
+                <table class="content-table">
+                    <thead>
+                        <tr>
+                            <th>STT</th>
+                            <th>mã giảm giá</th>
+                            <th>mô tả</th>
+                            <th>kích hoạt từ</th>
+                            <th>kích hoạt đến</th>
+                            <th>trạng thái</th>
+                            <th>hoạt động</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>APP10K</td>
+                            <td>Giảm 10k ngay lần đầu đăng kí app</td>
+                            <td>11/11/2022</td>
+                            <td>Không giới hạn</td>
+                            <td><span>Hoạt động</span></td>
+                            <td>
+                                <button>chỉnh sửa</button>
+                                <button>xóa</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>APP10K</td>
+                            <td>Giảm 10k ngay lần đầu đăng kí app</td>
+                            <td>11/11/2022</td>
+                            <td>Không giới hạn</td>
+                            <td><span>Hoạt động</span></td>
+                            <td>
+                                <button>chỉnh sửa</button>
+                                <button>xóa</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>APP10K</td>
+                            <td>Giảm 10k ngay lần đầu đăng kí app</td>
+                            <td>11/11/2022</td>
+                            <td>Không giới hạn</td>
+                            <td><span>Hoạt động</span></td>
+                            <td>
+                                <button>chỉnh sửa</button>
+                                <button>xóa</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+</div>
