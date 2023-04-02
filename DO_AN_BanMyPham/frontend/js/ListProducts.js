828a37cdd48662325ac22af6b63ab7a3dbfc0c5d
NTLinh <127573893+NTLinh194@users.noreply.github.com>
2023-04-02 22:39:00 +0700

    Update ListProducts.js

--- DO_AN_BanMyPham/frontend/js/ListProducts.js
+++ DO_AN_BanMyPham/frontend/js/ListProducts.js
@@ -1,7 +1,22 @@
-const slideshow = document.querySelector('.header__slideshow-adea');
-let counter = 0;
+const slideShow = document.querySelector('.header__slideshow-adea');
+let count = 0;
 
 setInterval(() => {
-    slideshow.style.transform = `translateX(${-counter * 100}%)`;
-    counter = (counter + 1) % 4;
+    slideShow.style.transform = `translateX(${-count * 100}%)`;
+    count = (count + 1) % 4;
 }, 3000);
+
+
+$(document).ready(function () {
+    $.ajax({
+        url: 'Models/fetch_data.php',
+        method: 'GET',
+        data: {
+            id: 1,
+        },
+        success: function (data) {
+            $("#home__product").html(data);
+        }
+    })
+})
+
