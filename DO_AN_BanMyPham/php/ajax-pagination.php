9cb6e196ed50a26a2b43f118c5df94e15f324391
NTLinh <127573893+NTLinh194@users.noreply.github.com>
2023-04-11 14:37:24 +0700

    Add files via upload

---
+++ DO_AN_BanMyPham/php/ajax-pagination.php
@@ -0,0 +1,24 @@
+<?php
+    include 'connect_Database.php';
+
+    $sql_total = "SELECT COUNT(*) as total FROM products";
+    $result_total = mysqli_query($conn,$sql_total);
+    $row_total = mysqli_fetch_assoc($result_total);
+    $total_record = $row_total['total'];
+    
+    $total_pages = ceil($total_record/$limit_per_page);
+    if ($total_pages > 1) {
+        for ($i = 1; $i <= $total_pages; $i++) {
+            echo '<li class="pagination-item" id="'. $i .'">';
+            if ($i == $page) {
+                echo  '<a href="#" class="pagination-item__link pagination-item__link--active">' . $i . '</a>';
+            } 
+            else {
+                echo '<a href="#" id="' . $i . '" class="pagination-item__link">' . $i . '</a>';
+            }
+            echo '</li>';
+        }
+    }
+
+    mysqli_close($conn);
+?>
---
+++ DO_AN_BanMyPham/php/indexListProducts.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/css/base.css">
+    <link rel="stylesheet" href="../assets/css/style_ListProducts.css">
+
+</head>
+<body>
+    <div class="app">
+        <div class="header__slideshow">
+            <div class="header__slideshow-adea">
+                <div class="header__slideshow-image">
+                    <img class="header__slideshow-image-item" src="../assets/img/slide3.jpg" alt="slide 1">
+                </div>
+    
+                <div class="header__slideshow-image">
+                    <img class="header__slideshow-image-item" src="../assets/img/slide4.jpg" alt="slide 2">
+                </div>
+    
+                <div class="header__slideshow-image">
+                    <img class="header__slideshow-image-item" src="../assets/img/slide5.jpg" alt="slide 3">
+                </div>
+    
+                <div class="header__slideshow-image">
+                    <img class="header__slideshow-image-item" src="../assets/img/slide6.jpg" alt="slide 4">
+                </div>
+
+                <div class="header__slideshow-image">
+                    <img class="header__slideshow-image-item" src="../assets/img/slide7.jpg" alt="slide 5">
+                </div>
+
+                <div class="header__slideshow-image">
+                    <img class="header__slideshow-image-item" src="../assets/img/slide8.jpg" alt="slide 6">
+                </div>
+            </div>
+        </div>
+
+        <div class="app__container">
+            <div class="app__container__name-shop">shop</div>
+
+            <div class="grid">
+                <div class="grid__row app__content">
+                    <div class="grid__column-3">
+                        <nav class="category">
+                            <h3 class="home__page">Trang chủ
+                                <b>/</b>
+                                <a href="#" class="shop__page">Shop</a>
+                            </h3>
+
+                            <div class="classification">
+                                <span class="category-list-title">Danh mục</span>
+                                <ul class="category-list" id="list__category">
+                                    
+                                </ul>
+
+                                <span class="category-list-title">Thương hiệu</span>
+                                <ul class="category-list" id="list__brand">
+                                    
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
+    
+                    <div class="grid__column-9">
+                        <div class="select__search">
+                            <select name="" id="sortPrices" onchange="onSort();">
+                                <option>Sắp xếp giá</option>
+                                <option value="1">Từ thấp đến cao</option>
+                                <option value="2">Từ cao đến thấp</option>
+                            </select>
+                        </div>
+                        <!-- Show Products -->
+                        <div class="home-product">
+                            <div class="grid__row" id="home__product">
+                                
+                            </div>
+                        </div>
+
+                        <!-- pagination -->
+                        <ul class="pagination home-product__pagination" id="number-page">
+                        <!--                         
+                            <li class="pagination-item">
+                                <a href="" id="1" class="pagination-item__link">
+                                    <i class="pagination-item__icon fas fa-angle-left"></i>
+                                </a>
+                            </li>
+                            <li class="pagination-item">
+                                <a href="" id="2" class="pagination-item__link pagination-item__link--active">1</a>
+                            </li>
+                            <li class="pagination-item">
+                                <a href="" id="3" class="pagination-item__link">2</a>
+                            </li>
+                            <li class="pagination-item">
+                                <a href="" id="4" class="pagination-item__link">3</a>
+                            </li>
+                            
+                            <li class="pagination-item">
+                                <a href="" id="9" class="pagination-item__link">
+                                    <i class="pagination-item__icon fas fa-angle-right"></i>
+                                </a>
+                            </li> 
+                         -->
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
+    <script src="../assets/js/indexProduct.js"></script>
+
+</body>
+</html>
