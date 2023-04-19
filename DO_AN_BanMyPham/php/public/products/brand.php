69991d14d31922c35eeb6965be7c8e73521e11d0
NTLinh <127573893+NTLinh194@users.noreply.github.com>
2023-04-19 08:19:55 +0700

    Update brand.php

--- DO_AN_BanMyPham/php/public/products/brand.php
+++ DO_AN_BanMyPham/php/public/products/brand.php
@@ -5,26 +5,18 @@
         $sqlth = "SELECT * FROM brands";
         $resultth =mysqli_query($conn,$sqlth);
 
-        $brands = array();
-
         if(mysqli_num_rows($resultth) > 0){
             while ($row = mysqli_fetch_assoc($resultth)) {
-                $brands[] = $row;
-            }
-        }
-        foreach($brands as $row) {
-            echo    '<li class="category-item">
+                echo    '<li class="category-item">
                         <div bid="'. $row["BRAND_ID"] .'" class="category-item__link select-brand">'. $row["NAME_BRAND"] .'</div>
                     </li>';
+            }
         }
     }
 
     else {
         echo "Không tìm thấy!";
     }
-
-
-    
 
     // Đóng kết nối  
     mysqli_close($conn);
