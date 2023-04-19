0c66e84528acdc8e641cfa6471a853189c2b91e0
NTLinh <127573893+NTLinh194@users.noreply.github.com>
2023-04-19 08:17:18 +0700

    Update indexListProducts.php

--- DO_AN_BanMyPham/php/public/products/indexListProducts.php
+++ DO_AN_BanMyPham/php/public/products/indexListProducts.php
@@ -1,5 +1,5 @@
-<div class="app">
-        <!-- <div class="header__slideshow">
+<div class="app" id="main-listProduct">
+        <div class="header__slideshow">
             <div class="header__slideshow-adea">
                 <div class="header__slideshow-image">
                     <img class="header__slideshow-image-item" src="../assets/img/slide3.jpg" alt="slide 1">
@@ -25,7 +25,7 @@
                     <img class="header__slideshow-image-item" src="../assets/img/slide8.jpg" alt="slide 6">
                 </div>
             </div>
-        </div> -->
+        </div>
 
         <div class="app__container">
             <div class="app__container__name-shop">shop</div>
@@ -55,8 +55,8 @@
     
                     <div class="grid__column-9">
                         <div class="select__search">
-                            <select name="" id="sortPrices" onchange="onSort();">
-                                <option>Sắp xếp giá</option>
+                            <select name="" id="sortPrices">
+                                <option value="0">Sắp xếp giá</option>
                                 <option value="1">Từ thấp đến cao</option>
                                 <option value="2">Từ cao đến thấp</option>
                             </select>
@@ -64,36 +64,16 @@
                         <!-- Show Products -->
                         <div class="home-product">
                             <div class="grid__row" id="home__product">
+                                
                             </div>
                         </div>
 
                         <!-- pagination -->
                         <ul class="pagination home-product__pagination" id="number-page">
-                        <!--                         
-                            <li class="pagination-item">
-                                <a href="" id="1" class="pagination-item__link">
-                                    <i class="pagination-item__icon fas fa-angle-left"></i>
-                                </a>
-                            </li>
-                            <li class="pagination-item">
-                                <a href="" id="2" class="pagination-item__link pagination-item__link--active">1</a>
-                            </li>
-                            <li class="pagination-item">
-                                <a href="" id="3" class="pagination-item__link">2</a>
-                            </li>
-                            <li class="pagination-item">
-                                <a href="" id="4" class="pagination-item__link">3</a>
-                            </li>
-                            
-                            <li class="pagination-item">
-                                <a href="" id="9" class="pagination-item__link">
-                                    <i class="pagination-item__icon fas fa-angle-right"></i>
-                                </a>
-                            </li> 
-                         -->
+                       
                         </ul>
                     </div>
                 </div>
             </div>
         </div>
-</div>
+    </div>
