7f781d1309b3c6e317ec732f5de87819432eb214
NTLinh <127573893+NTLinh194@users.noreply.github.com>
2023-04-21 21:33:58 +0700

    Update brand.php

--- DO_AN_BanMyPham/php/public/products/brand.php
+++ DO_AN_BanMyPham/php/public/products/brand.php
@@ -2,14 +2,21 @@
     include 'connect_Database.php';
 
     if(isset($_GET['brand'])) {
-        $sqlth = "SELECT * FROM brands";
+        $sqlth = "SELECT NAME_BRAND FROM brands";
         $resultth =mysqli_query($conn,$sqlth);
-
+        $id_brand = isset($_GET['id_brand']) ? $_GET['id_brand']: 0;
         if(mysqli_num_rows($resultth) > 0){
+            $stt = 1;
             while ($row = mysqli_fetch_assoc($resultth)) {
-                echo    '<li class="category-item">
-                        <div bid="'. $row["BRAND_ID"] .'" class="category-item__link select-brand">'. $row["NAME_BRAND"] .'</div>
-                    </li>';
+                echo    '<li class="category-item" brandid ='. $stt .'>';
+                if($id_brand != $stt){
+                    echo    '<div bid="'. $row["BRAND_ID"] .'" class="category-item__link"  brandid ='. $stt .'>'. $row["NAME_BRAND"] .'</div>';
+                }
+                else {
+                    echo    '<div bid="'. $row["BRAND_ID"] .'" class="category-item__link select-brand">'. $row["NAME_BRAND"] .'</div>';
+                }
+                echo    '</li>';
+                $stt++;
             }
         }
     }
